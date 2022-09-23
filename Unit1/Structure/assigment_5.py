2b3b9567e40804d7adebbc1cfb2f50299b3dd3ed
LuisMCota <luis.f.m.c@outlook.com>
2022-09-23 10:33:34 -0500

    commmit

--- Unit1/Structure/assigment_5.py
+++ Unit1/Structure/assigment_5.py
@@ -5,26 +5,26 @@ indice, a.pop() elimina y retorna el ultimo elemento de la lista.
 '''
 from bisect import insort_right
 
-
+# definimos una funcion en donde se va a poner la raiz del arbol y va a agregar dos espacios 
 def ArbolBinario(raiz):
     return [raiz,[],[]]
 
-def insertIzquierda(raiz,nuevoValor):
-    rama_izquierda = raiz.pop(1)
-    if len(rama_izquierda) > 1:
+def insertIzquierda(raiz,nuevoValor):#esta funcion sirve para agregar un valor a la raiz que ya definimos
+    rama_izquierda = raiz.pop(1)# va a introducir un valor al arbol del lado izquierdo recorriendo un espacio con la funcion pop
+    if len(rama_izquierda) > 1:# esirve para recorrer el arreglo y insertar el valor, luego recorrer la posicion de los valores dentro de la rama
         raiz.insert(1,[nuevoValor,rama_izquierda,[]])
     else:
         raiz.insert(1, [nuevoValor,[],[]])
         return raiz
 
-def insertDerecha (raiz,nuevoValor):
-    rama_derecha = raiz.pop(2)
-    if len(rama_derecha) > 1:
+def insertDerecha (raiz,nuevoValor):# va a agregar un avalor a la derecha de la raiz que ya definimos
+    rama_derecha = raiz.pop(2)# va a empujar un espacio en el lado derecho para agregar el nuevo valos
+    if len(rama_derecha) > 1:# aqui va a recorrer el arreglo y va a insertar el nuevo valor empujando a los demas valores dentro de la rama
         raiz.insert(2,[nuevoValor,[],rama_derecha])
     else:
         raiz.insert(2,[nuevoValor,[],[]])
         return raiz
-
+#Probamos nuestras funciones creando una raiz y las ramas de estas
 arbol = ArbolBinario('A')
 insertIzquierda(arbol, 'B')
 insertDerecha(arbol, 'C')
@@ -33,6 +33,6 @@ insertIzquierda(arbol, 'E')
 insertDerecha(arbol, 'F')
 insertDerecha(arbol, 'G')
 
-print(arbol)
+print(arbol)#lo imprimimos 
 
-#['A', ['E', ['D', ['B', [], []], []], []], ['G', [], ['F', [], ['C', [], []]]]]
+# resultado = ['A', ['E', ['D', ['B', [], []], []], []], ['G', [], ['F', [], ['C', [], []]]]]
