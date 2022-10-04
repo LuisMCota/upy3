d122c0e96712dde2baaa3763930897a9e6149b68
LuisMCota <luis.f.m.c@outlook.com>
2022-10-04 13:55:19 -0500

    commit

--- Unit1/paradigms/pinky.py
+++ Unit1/paradigms/pinky.py
@@ -7,8 +7,7 @@ class animals():
         self.especie = str(input('\nName of the specie => '))
         self.IQ = int(input('Enter the IQ => '))
         self.habilitie_1 = str(input('Enter your habilities => '))
-        self.habilitie_2 = None
-        animals.count += 1
+        self.habilitie_2 = []
 
     def prints(self):
         data = {'Name':[self.especie],
@@ -25,32 +24,55 @@ animal_1.prints()
 animal_2.prints()
 animal_3.prints()
 
-def superior_intelligence(iq_animal, specie):
-    answer = int(input('\nHow much you want to increase your iq? '))
-    iq_animal = answer + iq_animal
-    print('\n',specie, 'have now', iq_animal, 'of iq')
 
-def pinkify(habilities):
+
+def superior_intelligence(animal):
+    answer = int(input('\nHow much you want to increase your iq? '))
+    animal.IQ = answer + animal.IQ
+    print(animal.especie, 'have now', animal.IQ, 'of iq')
+
+def pinkify(animal):
     answer = str(input('You want to eliminate all the habilities? '))
     if answer == 'yes':
-        habilities = habilities.split(' ').pop(len(habilities.split(' '))-1) 
-        print(habilities)
+        animal.habilitie_1 = None
+        animal.habilitie_2 = None
+        animal.prints()
     else:
-        print('OK')
+        print('Going back...')
 
-def super_power(especie, habilities_2, iq_animal):
-    if especie == 'elephant':
-        habilities_2 = 'Not be afraid of mice!'
-        
-    elif especie == 'mouse' and iq_animal > 100:
-        habilities = 'Speak'
-        print(habilities)
+def super_power(animal):
+    if animal.especie == 'elephant':
+        animal.habilitie_2 = 'Not be afraid of mice!'
+        animal.prints()
+    elif animal.especie == 'mouse' and animal.IQ > 100:
+        animal.habilitie_2 = 'Speak'
+        animal.prints()
     else:
-        print('Regresa al menu')
+        print('Going back...')
 
+def criteria(animal):
+    if animal.habilitie_2 == 'speak' or animal.IQ > 60:
+        print(animal.especie, 'is anthropomorfic!')
+    else:
+        print(animal.especie, 'its not anthropomorfic!')
+
+def not_So_Sane():
+    inp = str(input('Put the pinkieska word => '))
+    def contar_vocales(input):
+        contador = 0
+        for letra in input:
+            if letra.lower() in "aeiouAEIOU":
+                contador += 1
+        return contador
+
+    output = contar_vocales(inp)
+    if len(inp) <= 4 and output == 1:
+        print('Do', inp,'!!!','\nTRUE')
+    else:
+        print('It is not a pinkiesca word')
 
 answer = str(input('\nDo you want to transform one of the animals? '))
-if answer == 'yes':
+if answer == 'Yes' or 'yes' or 'Y' or 'y':
     print('\n----------- TRANSFORMATION -----------')
     question_1 = int(input('\nWhat do you want to transform?\n 1. Superior Intelligence\n 2. Pinkify\n 3. Super powers\n 4. Exit\n'))
     while question_1 != 4:
@@ -59,45 +81,64 @@ if answer == 'yes':
             question_2 = int(input('\nChoose an animal:\n 1. Experimento 1\n 2. Experimento 2\n 3. Experimento 3\n 4. Regresar\n'))
             while question_2 != 4:
                 if question_2 == 1:    
-                    superior_intelligence(animal_1.IQ, animal_1.especie)
+                    superior_intelligence(animal_1)
                 elif question_2 == 2:
-                    superior_intelligence(animal_2.IQ,animal_2.especie)
+                    superior_intelligence(animal_2)
                 elif question_2 == 3:
-                    superior_intelligence(animal_3.IQ,animal_3.especie)
+                    superior_intelligence(animal_3)
                 else:
-                    print('Put a correct animal')  
-                question_2 = int(input('\nChoose an animal:\n 1. Experimento 1\n 2. Experimento 2\n 3. Experimento 3\n 4. Regresar\n'))
+                    print('Put a number of the list!!!')  
+                question_2 = int(input('\nChoose an animal:\n 1. Experiment 1\n 2. Experiment 2\n 3. Experiment 3\n 4. Regresar\n'))
         elif question_1 == 2:
             print('\n----------- PINKIFY -----------')
-            question_3 = int(input('\nChoose an animal:\n 1. Experimento 1\n 2. Experimento 2\n 3. Experimento3\n 4. Regresar\n'))
+            question_3 = int(input('\nChoose an animal:\n 1. Experiment 1\n 2. Experiment 2\n 3. Experiment 3\n 4. Regresar\n'))
             while question_3 != 4:
                 if question_3 == 1:  
-                    pinkify(animal_1.habilitie_1)  
+                    pinkify(animal_1)
                 elif question_3 == 2:
-                    pinkify(animal_2.habilitie_1)  
+                    pinkify(animal_2)  
                 elif question_3 == 3:
-                    pinkify(animal_3.habilitie_1)  
+                    pinkify(animal_3)  
                 else:
-                    print('Put a correct animal')  
-                question_3 = int(input('\nChoose an animal:\n 1. Experimento 1\n 2. Experimento 2\n 3. Experimento 3\n 4. Regresar\n')) 
+                    print('Put a number of the list!!!')  
+                question_3 = int(input('\nChoose an animal:\n 1. Experiment 1\n 2. Experiment 2\n 3. Experiment 3\n 4. Regresar\n'))
         elif question_1 == 3:
             print('\n----------- SUPER POWERS -----------')
-            question_4 = int(input('\nChoose an animal:\n 1. Experimento 1\n 2. Experimento 2\n 3. Experimento 3\n 4. Regresar\n'))
+            question_4 = int(input('\nChoose an animal:\n 1. Experiment 1\n 2. Experiment 2\n 3. Experiment 3\n 4. Regresar\n'))
             while question_4 != 4:
                 if question_4 == 1: 
-                    super_power(animal_1.especie, animal_1.habilitie_2, animal_1.IQ) 
-                    animal_1.prints()
+                    super_power(animal_1)
                 elif question_4 == 2:
-                    pinkify(animal_2.habilitie_1)  
+                    super_power(animal_2) 
                 elif question_4 == 3:
-                    pinkify(animal_3.habilitie_1)  
+                    super_power(animal_3) 
                 else:
-                    print('Put a correct animal')  
-                question_4 = int(input('\nChoose an animal:\n 1. Experimento 1\n 2. Experimento 2\n 3. Experimento 3\n 4. Regresar\n'))
-            
+                    print('Put a number of the list!!!')  
+                question_4 = int(input('\nChoose an animal:\n 1. Experiment 1\n 2. Experiment 2\n 3. Experiment 3\n 4. Regresar\n'))
         else:
-            print('')
+            print('Put one number of the list!!!')
         question_1 = int(input('\nWhat do you want to transform?\n 1. Superior Intelligence\n 2. Pinkify\n 3. Super powers\n 4. Exit\n'))
 else:
-    print('')
-        
+    print('OKAY CONTINUE...')
+
+
+ans_1 = str(input('\nDo you want to know if your animal is suitable for transformation? '))
+if ans_1 == 'Yes' or 'yes' or 'Y' or 'y':
+    ans_2 = int(input('\nChoose an animal:\n 1. Experiment 1\n 2. Experiment 2\n 3. Experiment 3\n 4. Exit\n'))
+    while ans_2 != 4:
+        if ans_2 == 1:
+            criteria(animal_1)
+        elif ans_2 == 2:
+            criteria(animal_2)
+        elif ans_2 == 3:
+            criteria(animal_3)
+        else:
+            print('Put one number of the list! ')
+        ans_2 = int(input('\nChoose an animal:\n 1. Experiment 1\n 2. Experiment 2\n 3. Experiment 3\n 4. Exit\n'))
+elif ans_1 == 'NO' or 'no' or 'n' or 'N':
+    print('Its important to know this')
+else:
+    print('Its important to know this')
+
+print('To continue, create a pinkiesca word!')
+not_So_Sane()
