6728c9f1a3ea8b9c84ed27e6f8f1eb2f9291ace1
LuisMCota <luis.f.m.c@outlook.com>
2022-10-05 12:10:58 -0500

    Commit

--- Unit1/paradigms/pinky.py
+++ Unit1/paradigms/pinky.py
@@ -7,7 +7,7 @@ class animals():
         self.especie = str(input('\nName of the specie => '))
         self.IQ = int(input('Enter the IQ => '))
         self.habilitie_1 = str(input('Enter your habilities => '))
-        self.habilitie_2 = []
+        self.habilitie_2 = None
 
     def prints(self):
         data = {'Name':[self.especie],
@@ -23,8 +23,6 @@ animal_3 = animals()
 animal_1.prints()
 animal_2.prints()
 animal_3.prints()
-
-
 
 def superior_intelligence(animal):
     answer = int(input('\nHow much you want to increase your iq? '))
@@ -52,9 +50,9 @@ def super_power(animal):
 
 def criteria(animal):
     if animal.habilitie_2 == 'speak' or animal.IQ > 60:
-        print(animal.especie, 'is anthropomorfic!')
+        print(animal.especie, 'is anthropomorfic!\nIt is suitable for experimentation!')
     else:
-        print(animal.especie, 'its not anthropomorfic!')
+        print(animal.especie, 'is not anthropomorfic!\nNot suitable for experimentation!')
 
 def not_So_Sane():
     inp = str(input('Put the pinkieska word => '))
@@ -71,8 +69,10 @@ def not_So_Sane():
     else:
         print('It is not a pinkiesca word')
 
+
+
 answer = str(input('\nDo you want to transform one of the animals? '))
-if answer == 'Yes' or 'yes' or 'Y' or 'y':
+if answer == 'Yes' or answer == 'yes' or answer == 'Y' or answer == 'y':
     print('\n----------- TRANSFORMATION -----------')
     question_1 = int(input('\nWhat do you want to transform?\n 1. Superior Intelligence\n 2. Pinkify\n 3. Super powers\n 4. Exit\n'))
     while question_1 != 4:
@@ -121,9 +121,8 @@ if answer == 'Yes' or 'yes' or 'Y' or 'y':
 else:
     print('OKAY CONTINUE...')
 
-
 ans_1 = str(input('\nDo you want to know if your animal is suitable for transformation? '))
-if ans_1 == 'Yes' or 'yes' or 'Y' or 'y':
+if ans_1 == 'Yes' or ans_1 == 'yes' or ans_1 == 'Y' or ans_1 == 'y':
     ans_2 = int(input('\nChoose an animal:\n 1. Experiment 1\n 2. Experiment 2\n 3. Experiment 3\n 4. Exit\n'))
     while ans_2 != 4:
         if ans_2 == 1:
@@ -135,10 +134,21 @@ if ans_1 == 'Yes' or 'yes' or 'Y' or 'y':
         else:
             print('Put one number of the list! ')
         ans_2 = int(input('\nChoose an animal:\n 1. Experiment 1\n 2. Experiment 2\n 3. Experiment 3\n 4. Exit\n'))
-elif ans_1 == 'NO' or 'no' or 'n' or 'N':
-    print('Its important to know this')
 else:
-    print('Its important to know this')
+    print('ITS IMPORTANT TO KNOW THIS!!\n')
 
-print('To continue, create a pinkiesca word!')
+print('\nTo continue, create a pinkiesca word!')
 not_So_Sane()
+
+print('\n------------------- RESULTS -------------------\n')
+animal_1.prints()
+print()
+criteria(animal_1)
+print('-----------------------------------------------')
+animal_2.prints()
+print()
+criteria(animal_2)
+print('-----------------------------------------------')
+animal_3.prints()
+print()
+criteria(animal_3)
