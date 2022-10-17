5d42429fb61888ed44de63d110cd0c87a668fcf8
LuisMCota <luis.f.m.c@outlook.com>
2022-10-17 11:45:06 -0500

    commit

--- Unit1/paradigms/tree.py
+++ Unit1/paradigms/tree.py
@@ -1,17 +1,62 @@
-COEFFICIENT = 45
+baobab = ['Baobab',5,10,1.7]
+maple = ['Maple',6,7,10]
 
-main = lambda species, height, width, vitality: (species, height, width, vitality)
+def frondoso(arbol):
+    if arbol[1] >= 6 and arbol[1] <= 15 and arbol[2] > arbol[1] and arbol[3] > 1:
+        print('El arbol',arbol[0],'es frondoso')
+    else:
+        print('El arbol',arbol[0],'no es frondoso')
 
-eachTreeisLeafy = lambda eachTree: eachTree[1] > 6 and eachTree[1] < 15 and eachTree[2] > eachTree[1] and eachTree[3] > 1
+def vida(arbol):
+    print('La esperanza de vida de tu arbol es de:',(arbol[3]*45)/2)
 
-life_exp= lambda eachTree: eachTree[3] * COEFFICIENT / 2
+def fact_climaticos(arbol):
+    def lluvia(arbol):
+        choose = str(input('Llovio? '))
+        if choose =='si' or choose == 'Si' or choose == 's':
+            lluvia = int(input('Cuantos mm llovio? '))
+            altura = arbol[1]+1
+            vitalidad = arbol[3]+lluvia%arbol[3]
+            print('Ahora',arbol[0],'mide', altura, 'y su vitalidad incremento a', round(vitalidad))
+        else:
+            print('OK')
 
-rain = lambda mm, eachTree: (eachTree[0], eachTree[1] + 1, eachTree[2], eachTree[3] + (eachTree[3] * mm / 100))
+    def granizo(arbol):
+        granizo = str(input('Granizo? '))
+        if granizo == 'si' or granizo == 'Si' or granizo == 's':
+            result = arbol[1] - 2
+            if result >= 1:
+                print('Ahora tu arbol mide', result)
+            else:
+                print('OK')
+    
+    def tormenta(arbol):
+        altura = arbol[1]+1-2
+        vitalidad = arbol[3]+arbol[3]
+        print('\nLos datos de {} quedan asi:\n 1. Altura => {}\n 2. Vitalidad => {}\n'.format(arbol[0], altura, vitalidad))
 
-hail = lambda eachTree: (eachTree[0], eachTree[1] - 2, eachTree[3], eachTree[4]) if eachTree[1] >= 3 else eachTree
+    lluvia(arbol)
+    granizo(arbol)
+    tormenta(arbol)
 
-storm = lambda eachTree: hail(rain(100, eachTree))
+def buen_dia(arbol):
+    result = 150%arbol[3]+arbol[3]
+    if result > 5:
+        print('Es un buen dia para tu arbol {} :) '.format(arbol[0]))
+    else:
+        print('Fue un mal dia para {} :('.format(arbol[0]))
 
-isGood = lambda eachTree: rain(150, eachTree)[3] > 5
 
-newBaobabeachTree = main("baobab", 5, 10, 1.7)
+print()
+frondoso(baobab)
+frondoso(maple)
+print()
+vida(baobab)
+vida(maple)
+print()
+fact_climaticos(baobab)
+fact_climaticos(maple)
+print()
+buen_dia(baobab)
+buen_dia(maple)
+print()
